bd263de8877da44bb6de6574225b0986de40497a
marcofig98 <marc0_fig@hotmail.com>
2025-03-14 13:51:13 +0000

    fixed emailConfig

--- src/main/java/com/management/demo/email/EmailConfig.java
+++ src/main/java/com/management/demo/email/EmailConfig.java
@@ -1,5 +1,6 @@
 package com.management.demo.email;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,19 +11,26 @@ import java.util.Properties;
 @Configuration
 public class EmailConfig {
 
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp-mail.outlook.com");
+        mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername(System.getenv("MAIL_USERNAME"));  // enviromnment variable
-        mailSender.setPassword(System.getenv("MAIL_PASSWORD"));  // enviromnment variable
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
-
         return mailSender;
     }
 }
+
