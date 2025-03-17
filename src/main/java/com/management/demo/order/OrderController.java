60381b171960d643553d2d81af8ce848d167aac3
marcofig98 <marc0_fig@hotmail.com>
2025-03-17 14:19:23 +0000

    added some information in controllers

--- src/main/java/com/management/demo/order/OrderController.java
+++ src/main/java/com/management/demo/order/OrderController.java
@@ -1,8 +1,8 @@
 package com.management.demo.order;
 
-import com.management.demo.stockmovement.StockMovement;
 import com.management.demo.stockmovement.StockMovementDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +13,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/orders")
+@Tag(name = "Orders", description = "Order Management controller")
 public class OrderController {
 
     private final OrderService orderService;
